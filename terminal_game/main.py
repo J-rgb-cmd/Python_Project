67d5a9fb8e50d0b324cc034f47ea4dd94fd81815
j-rgb-cmd <jackaria.hossain@infotechhs.net>
2023-05-21 16:18:13 -0400

    new version

--- terminal_game/main.py
+++ terminal_game/main.py
@@ -1,50 +1,74 @@
-default_matrix = [[1,2,3],[4,5,6],[7,8,9]]
+class Gameboard: 
+  def __init__(self):
+    self.matrix = [[1,2,3],[4,5,6],[7,8,9]]
 
-def print_screen(matrix):
-    for row in matrix:
+  def print_board(self):
+    for row in self.matrix:
         for index in row:
-            print(index, end='    ')
+            print(index, end = '    ')
         print('\n')
 
-user1_sign = ''
-user2_sign = ''
+  def edit(self, player, position):
+    if type(player) == Player and type(position) == int:
+      for i in range(len(self.matrix)):
+        for j in range(len(self.matrix[i])):
+          if self.matrix[i][j] == position and type(self.matrix[i][j]) == int:
+            self.matrix[i][j] = player.symbol
+        
+  def check(self):
+    for row in self.matrix:
+      counter = 0
+      for num in row:
+        if num == 'x':
+          counter += 1
+        if counter == 3:
+          return 'x'
+        elif counter == 0:
+          return 'o'
+        else: 
+          continue
+    for j in range(3):
+      counter = 0
+      for i in range(3):
+        if self.matrix[i][j] == 'x':
+          counter += 0
+      if counter == 3:
+          return 'x'
+      elif counter == 0:
+          return 'o'
+      else: 
+          continue
+    for i in range(3):
+      for j in range(3):
+        if i == j:
+          if self.matrix == 'x':
+            counter += 1
+          
+  def clear(self):
+    self.matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    return 'New Game!'
 
-user1_input = ''
-user2_input = ''
 
-try_again = ''
+gameboard = Gameboard()
 
-def user_selection():
-    bool_continue = False
-    while (bool_continue == False):
+class Player:
+  def __init__(self, symbol, name):
+    self.name = name
+    self.symbol = symbol
+    self.win = False
+    self.score = 0
 
-        choice = input("Select Shape: x or o\n")
-        if choice == 'x':
-            user1_sign = 'x'
-            user2_sign = 'o'
-            bool_continue = True
-        elif choice == 'o':
-            user1_sign = 'o'
-            user2_sign = 'x'
-            bool_continue = True
-        else:
-            print('Invalid Input, Try Again')
+  def win_status(self):
+    if gameboard.check() == 'x' and self.symbol == 'x':
+      self.win = True
+    elif gameboard.check() == 'o' and self.symbol == 'o':
+      self.win = True
+      
+token = True
 
-
-def select_location(input, sign):
-    for i in range(len(default_matrix)):
-        for j in range(len(default_matrix[i])):
-            if int(input) == default_matrix[i][j]:
-                default_matrix[i][j] = sign
-            
-    
-
-
-                    
-
-
-
-user1_input= input('Select grid \n')
-select_location(user1_input,'x')
-print_screen(default_matrix)
-
+   
+gameboard.print_board()
+player1 = Player('x', 'Jack')
+gameboard.edit(player1, 10)
+gameboard.print_board()
+  
